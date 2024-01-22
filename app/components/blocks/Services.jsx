8fe4f61450f874bcc023e31cb5108d026d4c34c3
darshaka1234 <darshakasrimal1234@gmail.com>
2024-01-23 02:17:44 +0530

    block 2

---
+++ app/components/blocks/Services.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Image from "next/image";
+
+const Services = () => {
+  const data = [
+    {
+      title: "Custom Software",
+      desc: "We create solutions that solve your business problems. Our team has a decade long success track record.",
+      icon: <Image src="/computer.png" alt="computer" layout="fill" />,
+    },
+    {
+      title: "Web Applications",
+      desc: "We typically build all web applications from scratch to satisfy your requirements.",
+      icon: <Image src="/bracket.png" alt="computer" layout="fill" />,
+    },
+    {
+      title: "Mobile Applications",
+      desc: "We craft better user experience and Interface designs for your digital products.",
+      icon: <Image src="/weather.png" alt="computer" layout="fill" />,
+    },
+    {
+      title: "UI / UX Design",
+      desc: "We craft better user experience and Interface designs for your digital products.",
+      icon: <Image src="/ux-design.png" alt="computer" layout="fill" />,
+    },
+  ];
+
+  const text =
+    "We are committed to keeping you ahead in today’s fast-paced and" +
+    "ever-evolving world by delivering digital solutions that solve problems" +
+    "in a secure, simple and effective way.";
+  return (
+    <div className="bg-black text-white pt-28 pr-28">
+      <h1 className="">{text}</h1>
+      <div className="grid grid-cols-2 gap-10 mt-12">
+        {data.map((i) => {
+          return (
+            <div key={i.title} className="flex flex-col gap-5">
+              <div key={i.title} className="relative w-10 h-10">
+                <div>{i.icon}</div>
+              </div>
+              <h1>{i.title}</h1>
+              <h1>{i.desc}</h1>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Image from "next/image";
 import Landing from "./components/blocks/Landing";
+import Services from "./components/blocks/Services";
 
 const page = () => {
   return (
@@ -11,8 +12,14 @@ const page = () => {
         <div className="relative h-[100vh] ">
           <Image src="/One.png" alt="image 1" layout="fill" />
         </div>
-
         <Landing />
+        <div className="flex flex-col h-[100vh] bg-black px-28 pt-28 gap-3">
+          <h4 className="text-[#717171]">WHAT WE DO</h4>
+          <h1 className="text-white text-5xl font-bold leading-normal">
+            Integrated tech solutions across all digital platforms
+          </h1>
+        </div>
+        <Services />
       </div>
     </div>
   );
