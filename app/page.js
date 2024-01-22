3f1cddfe7b74758237e5386ea87d757dec0fc2a7
darshaka1234 <darshakasrimal1234@gmail.com>
2024-01-23 01:31:23 +0530

    block one ok

--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-
+import Image from "next/image";
+import Landing from "./components/blocks/Landing";
 
 const page = () => {
   return (
     <div>
       <Navbar />
+      <div className="grid grid-cols-2">
+        <div className="relative h-[100vh] ">
+          <Image src="/One.png" alt="image 1" layout="fill" />
+        </div>
+
+        <Landing />
+      </div>
     </div>
   );
 };
